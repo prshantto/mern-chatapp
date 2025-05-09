2ba9942955e9af73d37a5576937efc76c551b832
prashant <prashanttt001@gmail.com>
2025-05-09 17:18:35 +0530

    created login api route

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -5,13 +5,12 @@ export const checkAuth = (req: Request, res: Response) => {
   res.send("yo! server is running using typescript...");
 };
 
-interface newUser {
-  fullName: string;
-  email: string;
-  password: string;
-}
-
 export const register = async (req: Request, res: Response) => {
+  interface newUser {
+    fullName: string;
+    email: string;
+    password: string;
+  }
   const { fullName, email, password }: newUser = req.body;
   try {
     if (!fullName || !email || !password) {
@@ -51,8 +50,38 @@ export const register = async (req: Request, res: Response) => {
   }
 };
 
-export const login = (req: Request, res: Response) => {
-  res.send("yo! server is running using typescript...");
+export const login = async (req: Request, res: Response) => {
+  interface user {
+    email: string;
+    password: string;
+  }
+  const { email, password }: user = req.body;
+
+  try {
+    if (!email || !password) {
+      res.status(501).json({ message: "All fields are required" });
+    }
+    if (password.length < 6) {
+      res
+        .status(501)
+        .json({ message: "Password must consist of 6 characters " });
+    }
+
+    // find user and compare password
+
+    const user = await User.findOne({ email }).select("+password");
+    //comapre password using bcrypt
+    if (user?.password != password) {
+      res.status(501).json({ message: "Invalid Credentials" });
+    }
+
+    res.status(201).json({ message: "User loggedin sucessfully", User: user });
+  } catch (error) {
+    console.log(
+      `some error occured at login route, error: ${(error as Error).message}`
+    );
+    res.status(501).json({ message: "Internal server error" });
+  }
 };
 
 export const logout = (req: Request, res: Response) => {
